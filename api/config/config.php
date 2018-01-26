2aa11d3e470648bb4a7edb9ef233d4c5198109a8
Mac Diphot <diphot@iMac.local>
2018-01-26 18:14:48 -0300

    Routes modif

--- api/config/config.php
+++ api/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
 * --------------------------------------------------------------------------
 * CONFIGURACIÓN GENERAL
 * --------------------------------------------------------------------------
@@ -11,21 +11,21 @@ return array(
 
 	/*
 	* ENVIRONMENT
-	* 
-	* El entorno que elijas va a afectar algunas configuraciones como el 
+	*
+	* El entorno que elijas va a afectar algunas configuraciones como el
 	* uso de logs de errores y si se muestran o no en pantalla.
-	* 
+	*
 	* Opciones: desarrollo, produccion
-	* 
+	*
 	*/
 	'enviroment' => 'produccion',
 
-	/* 
+	/*
 	* DOMINIOS PERMITIDOS
 	*
-	* Cargá todos los dominios desde los que querés que se vea el sitio. Si 
-	* se usa alguno de estos dominios la URL_BASE se va a construir 
-	* automáticamente pero si no el sitio da error. (Esto es una medida de 
+	* Cargá todos los dominios desde los que querés que se vea el sitio. Si
+	* se usa alguno de estos dominios la URL_BASE se va a construir
+	* automáticamente pero si no el sitio da error. (Esto es una medida de
 	* seguridad).
 	*
 	*/
@@ -35,33 +35,33 @@ return array(
 		'autopartgo.herokuapp.com'
 	),
 
-	/* 
+	/*
 	* URL BASE
-	* 
-	* Todas las URLs del sitio van a empezar con esta URL. Si no la 
+	*
+	* Todas las URLs del sitio van a empezar con esta URL. Si no la
 	* definís se construye automáticamente usando los dominios permitidos.
-	* 
-	* Esta variable está acá solo por si hace falta sobreescribir el valor 
+	*
+	* Esta variable está acá solo por si hace falta sobreescribir el valor
 	* que se genera automáticamente.
 	*
 	*/
-	// 'base_url' => 'http://192.168.0.152/dashboard/work/AutoPartGo/',
-	'base_url' => 'http://localhost:8888/weland/AutoPartGo/',
+	// 'base_url' => 'http://localhost:8888/weland/AutoPartGo/',
+	'base_url' => '',
 
-	/* 
+	/*
 	* RUTA BASE
-	* 
-	* Se calcula automáticamente, normalmente no hace falta definirla 
+	*
+	* Se calcula automáticamente, normalmente no hace falta definirla
 	* manualmente.
 	*
 	*/
 	'base_path' => '',
 
-	/* 
+	/*
 	* ZONA HORARIA
-	* 
+	*
 	* Si el sitio está en Argentina no hace falta cambiarla.
-	* 
+	*
 	*/
 	'default_timezone_set' => 'America/Argentina/Buenos_Aires',
 
